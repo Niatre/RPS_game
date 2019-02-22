55ffcdcfb6230f78cd7e8f709edf28e11c05a5dd
niatre <niatree9999@gmail.com>
2019-02-22 16:22:34 +0100

    added playRound function

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-let playerSelection = prompt ('Do you choose rock, paper or scissors');
-
+let playerSelection = prompt ('Do you choose rock, paper or scissors ? ');
 
 function computerPlay (){
-    let computerSelection = Math.floor(Math.random()*3)
-    if (computerSelection ==0 ){
-    return 'Rock'
+    let computerSelection = Math.floor(Math.random()*3);
+    if (computerSelection == 0 ){
+    return 'rock';
     } else if (computerSelection == 1){
-    return 'Paper'
+    return 'paper';
     } else {
-    return 'Scissors'
+    return 'scissors';
     }
+}
+
+function playRound(playerSelection, computerSelection) {
+  if(playerSelection == computerSelection){
+    return ("It's a tie!");
+  } else if((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")){
+    return ("You Lose! " + computerSelection + " beats " + playerSelection);
+  } else {
+    return ("You Win! " + playerSelection + " beats " + computerSelection);
+  }
 }
 
 
 
-
-
-
-
-console.log('You chose ' + playerSelection)
-console.log('The computer choose ' + computerPlay())
-  
+console.log('You chose ' + playerSelection);
+const computerSelection = computerPlay();
+console.log('The computer choose ' + computerSelection);
+console.log(playRound(playerSelection, computerSelection));
