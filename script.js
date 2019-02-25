58cbb3cbf18de7ac445cb5b6a29c41ae51f493b2
niatre <niatree9999@gmail.com>
2019-02-25 18:28:56 +0100

    added game function

--- script.js
+++ script.js
@@ -14,9 +14,9 @@ function computerPlay (){
 }
 
 function playRound(playerSelection, computerSelection) {
-  if(playerSelection == computerSelection){
+  if(playerSelection === computerSelection){
     console.log("It's a tie!");
-    return 
+    return
   } else if((playerSelection == "rock" && computerSelection == "paper") || (playerSelection == "paper" && computerSelection == "scissors") || (playerSelection == "scissors" && computerSelection == "rock")){
     console.log("You Lose! " + computerSelection + " beats " + playerSelection)
     return 
@@ -26,10 +26,24 @@ function playRound(playerSelection, computerSelection) {
   }
 }
 
-
+function game(){
+  let score = 0;
+ for(i = 0; i<6; i++){
+  let playerSelection = promptPlayer()
+            score += playRound(playerSelection, computerPlay())
+}
+console.log("The game has ended")
+  if(ties == 0){
+      console.log("It was a tie")
+  } else if(computerScore < playerScore){
+      console.log("You lost. Your score was: " + playerScore)
+  } else {
+      console.log("You won! Your score was: " + computerScore)
+  }
+}
 
 const playerSelection = promptPlayer();
 console.log('You chose ' + playerSelection);
 const computerSelection = computerPlay();
 console.log('The computer choose ' + computerSelection);
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
